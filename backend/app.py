0b4b9f5822886f0af20e510783b678376214d38a
피윤아 <145113747+yunah02@users.noreply.github.com>
2026-04-15 16:29:48 +0000

    Flask 서버 설정 및 구조 업데이트

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
-print("Backend 시작")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "백엔드 서버 연결 성공!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    
